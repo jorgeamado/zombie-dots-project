d4344dd946c2c7198ac5038bb41b9c4c66a068d7
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2023-06-28 10:18:50 +0200

    finalize zombie rise system

--- Assets/Scripts/ComponentsAndTags/ZombieRiseAspect.cs
+++ Assets/Scripts/ComponentsAndTags/ZombieRiseAspect.cs
@@ -11,11 +11,18 @@ namespace ComponentsAndTags
         private readonly RefRW<LocalTransform> _localTransform;
         private readonly RefRO<ZombieRiseRate> _zombieRiseRate;
 
+        public bool IsAboveGround => _localTransform.ValueRO.Position.y >= 0;
         public void Rise(float deltaTime)
         {
             var valueRWPosition = math.up() * deltaTime * _zombieRiseRate.ValueRO.Value;
             _localTransform.ValueRW.Position += valueRWPosition;
         }
-        
+
+        public void SetAtGroundLevel()
+        {
+            var position = _localTransform.ValueRO.Position;
+            position.y = 0;
+            _localTransform.ValueRW.Position = position;
+        }
     }
 }
--- Assets/Scripts/Systems/ZombieRiseSystem.cs
+++ Assets/Scripts/Systems/ZombieRiseSystem.cs
@@ -9,13 +9,19 @@ namespace Systems
     [UpdateAfter(typeof(SpawnZombieSystem))]
     public partial struct ZombieRiseSystem : ISystem
     {
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<EndSimulationEntityCommandBufferSystem.Singleton>();
+        }
+
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
             var deltaTime = SystemAPI.Time.DeltaTime;
-
+            var ecbSingleton = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>();
             new ZombieRiseJob
             {
+                ECB = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged).AsParallelWriter(),
                 DeltaTime = deltaTime,
             }.ScheduleParallel();
         }
@@ -25,11 +31,15 @@ namespace Systems
     public partial struct ZombieRiseJob : IJobEntity
     {
         public float DeltaTime;
-
+        public EntityCommandBuffer.ParallelWriter ECB;
         [BurstCompile]
-        private void Execute(ZombieRiseAspect zombie)
+        private void Execute([ChunkIndexInQuery] int sortKey, ZombieRiseAspect zombie)
         {
             zombie.Rise(DeltaTime);
+            
+            if(!zombie.IsAboveGround) return;
+            zombie.SetAtGroundLevel();
+            ECB.RemoveComponent<ZombieRiseRate>(sortKey, zombie.Entity);
         }
     }
 }
